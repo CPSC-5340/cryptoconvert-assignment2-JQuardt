fe8a0cf0941074f4d88157e05553f628aea0b2f5
Jenna Marquardt <jsr0010@auburn.edu>
2024-03-22 08:08:29 -0500

    Fixed the navigation title.

--- Assignment2/ContentView.swift
+++ Assignment2/ContentView.swift
@@ -10,14 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            Text("Conversion App")
-                .font(.title)
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
             List {
                 NavigationLink("World Currency Exchange", destination: CurrencyConverterView())
                 NavigationLink("Crypto Exchange", destination: CryptoConverterView())
             }
-        }.navigationTitle("Conversion App")
+            .navigationTitle("Conversion App")
+        }
     }
 }
 
